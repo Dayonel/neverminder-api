91b1e645511dd21ac1929a6fb2d7abcb00d51a67
Dayo <dayonel@hotmail.es>
2024-02-11 15:07:33 +0100

    Untrack appsettings development

--- Neverminder/Program.cs
+++ Neverminder/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0.1",
+        Title = "Neverminder API",
+        Description = "Reminders app that sends notifications for your plans.",
+    });
+});
+
 
 var app = builder.Build();
 
